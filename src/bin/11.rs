c429c94544e0ba78b52f6a6e7c9019cf8b913a55
Ludwig Granstedt <71264282+ludegra@users.noreply.github.com>
2022-12-13 09:23:19 +0100

    started day 12

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -12,7 +12,7 @@ fn main() {
 enum Operation {
     Multiplied(u64),
     Added(u64),
-    Squared
+    Squared,
 }
 
 struct Monkey {
@@ -24,77 +24,78 @@ struct Monkey {
 
 fn solve(_input: impl Iterator<Item = String>, start: Instant) {
     let mut monkies = vec![
+        // Monkey {
+        //     items: vec![79, 98],
+        //     operation: Operation::Multiplied(19),
+        //     test: Box::new(|test_result| if test_result { 2 } else { 3 }),
+        //     test_value: 23,
+        // },
+        // Monkey {
+        //     items: vec![54, 65, 75, 74],
+        //     operation: Operation::Added(6),
+        //     test: Box::new(|test_result| if test_result { 2 } else { 0 }),
+        //     test_value: 19,
+        // },
+        // Monkey {
+        //     items: vec![79, 60, 97],
+        //     operation: Operation::Squared,
+        //     test: Box::new(|test_result| if test_result { 1 } else { 3 }),
+        //     test_value: 13,
+        // },
+        // Monkey {
+        //     items: vec![74],
+        //     operation: Operation::Added(3),
+        //     test: Box::new(|test_result| if test_result { 0 } else { 1 }),
+        //     test_value: 17,
+        // },
         Monkey {
-            items: vec![79, 98],
-            operation: Operation::Multiplied(19),
-            test: Box::new(|test_result| if test_result { 2 } else { 3 }),
-            test_value: 23,
+            items: vec![74, 64, 74, 63, 53],
+            operation: Operation::Multiplied(7),
+            test: Box::new(|test_result| if test_result { 1 } else { 6 }),
+            test_value: 5,
         },
         Monkey {
-            items: vec![54, 65, 75, 74],
-            operation: Operation::Added(6),
-            test: Box::new(|test_result| if test_result { 2 } else { 0 }),
-            test_value: 19,
-        },
-        Monkey {
-            items: vec![79, 60, 97],
+            items: vec![69, 99, 95, 62],
             operation: Operation::Squared,
-            test: Box::new(|test_result| if test_result { 1 } else { 3 }),
+            test: Box::new(|test_result| if test_result { 2 } else { 5 }),
+            test_value: 17,
+        },
+        Monkey {
+            items: vec![59, 81],
+            operation: Operation::Added(8),
+            test: Box::new(|test_result| if test_result { 4 } else { 3 }),
+            test_value: 7,
+        },
+        Monkey {
+            items: vec![50, 67, 63, 57, 63, 83, 97],
+            operation: Operation::Added(4),
+            test: Box::new(|test_result| if test_result { 0 } else { 7 }),
             test_value: 13,
         },
         Monkey {
-            items: vec![74],
+            items: vec![61, 94, 85, 52, 81, 90, 94, 70],
             operation: Operation::Added(3),
-            test: Box::new(|test_result| if test_result { 0 } else { 1 }),
-            test_value: 17,
-        }, // Monkey {
-           //     items: vec![74, 64, 74, 63, 53],
-           //     operation: Operation::Multiplied(7),
-           //     test: Box::new(|test_result| if test_result { 1 } else { 6 }),
-           //     test_value : 5,
-           // },
-           // Monkey {
-           //     items: vec![69, 99, 95, 62],
-           //     operation: Operation::Squared,
-           //     test: Box::new(|test_result| if test_result { 2 } else { 5 }),
-           //     test_value: 17
-           // },
-           // Monkey {
-           //     items: vec![59, 81],
-           //     operation: Operation::Added(8),
-           //     test: Box::new(|test_result| if test_result { 4 } else { 3 }),
-           //     test_value: 7
-           // },
-           // Monkey {
-           //     items: vec![50, 67, 63, 57, 63, 83, 97],
-           //     operation: Operation::Added(4),
-           //     test: Box::new(|test_result| if test_result { 0 } else { 7 }),
-           //     test_value: 13
-           // },
-           // Monkey {
-           //     items: vec![61, 94, 85, 52, 81, 90, 94, 70],
-           //     operation: Operation::Added(3),
-           //     test: Box::new(|test_result| if test_result { 7 } else { 3 }),
-           //     test_value: 19
-           // },
-           // Monkey {
-           //     items: vec![69],
-           //     operation: Operation::Added(5),
-           //     test: Box::new(|test_result| if test_result { 4 } else { 2 }),
-           //     test_value: 3
-           // },
-           // Monkey {
-           //     items: vec![54, 55, 58],
-           //     operation: Operation::Added(7),
-           //     test: Box::new(|test_result| if test_result { 1 } else { 5 }),
-           //     test_value: 11
-           // },
-           // Monkey {
-           //     items: vec![79, 51, 83, 88, 93, 76],
-           //     operation: Operation::Multiplied(3),
-           //     test: Box::new(|test_result| if test_result { 0 } else { 6 }),
-           //     test_value: 2
-           // },
+            test: Box::new(|test_result| if test_result { 7 } else { 3 }),
+            test_value: 19,
+        },
+        Monkey {
+            items: vec![69],
+            operation: Operation::Added(5),
+            test: Box::new(|test_result| if test_result { 4 } else { 2 }),
+            test_value: 3,
+        },
+        Monkey {
+            items: vec![54, 55, 58],
+            operation: Operation::Added(7),
+            test: Box::new(|test_result| if test_result { 1 } else { 5 }),
+            test_value: 11,
+        },
+        Monkey {
+            items: vec![79, 51, 83, 88, 93, 76],
+            operation: Operation::Multiplied(3),
+            test: Box::new(|test_result| if test_result { 0 } else { 6 }),
+            test_value: 2,
+        },
     ]
     .into_iter()
     .enumerate()
@@ -104,11 +105,41 @@ fn solve(_input: impl Iterator<Item = String>, start: Instant) {
 
     let mut active_scores: HashMap<usize, u32> = HashMap::new();
 
-    // TODO: logic
+    let max = monkies
+        .iter()
+        .fold(1, |acc, (_, monke)| acc * monke.test_value);
+
+    for _ in 0..10000 {
+        for key in 0..monkies.keys().len() {
+            let current_monkey = monkies.get_mut(&key).unwrap();
+            let mut next_monkies: HashMap<u64, Vec<u64>> = HashMap::new();
+            while !current_monkey.items.is_empty() {
+                let item = current_monkey.items.remove(0);
+                let mut new_fear_level = match current_monkey.operation {
+                    Operation::Added(value) => item + value,
+                    Operation::Multiplied(value) => item * value,
+                    Operation::Squared => item * item,
+                };
+                new_fear_level %= max;
+                let next_monkey =
+                    (current_monkey.test)(new_fear_level % current_monkey.test_value == 0);
+                next_monkies
+                    .entry(next_monkey)
+                    .or_default()
+                    .push(new_fear_level);
+                let active_score = active_scores.entry(key).or_default();
+                *active_score += 1;
+            }
+            for (k, v) in next_monkies {
+                let next_monkey = monkies.get_mut(&(k as usize)).unwrap();
+                next_monkey.items.extend(&v);
+            }
+        }
+    }
 
     println!("{:?}", active_scores);
     let mut sorted = active_scores.values().map(|v| v.to_owned()).sorted().rev();
-    let product = sorted.next().unwrap() * sorted.next().unwrap();
+    let product = sorted.next().unwrap() as u128 * sorted.next().unwrap() as u128;
 
     println!("{}", product);
 }
@@ -203,8 +234,8 @@ fn part1() {
                 let new_fear_level = match current_monkey.operation {
                     Operation::Added(value) => item + value,
                     Operation::Multiplied(value) => item * value,
-                    Operation::Squared => item * item
-                };
+                    Operation::Squared => item * item,
+                } / 3;
                 println!("\t{} -> {}", item, new_fear_level);
                 let next_monkey =
                     (current_monkey.test)(new_fear_level % current_monkey.test_value == 0);
--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,4 +1,4 @@
-use std::time::Instant;
+use std::{time::Instant, collections::HashMap};
 const DAY_NUMBER: u32 = 12;
 
 fn main() {
@@ -7,6 +7,96 @@ fn main() {
     solve(input, start);
 }
 
-fn solve(input: impl Iterator<Item = String>, start: Instant) {
+#[derive(Debug, Clone)]
+struct Node {
+    pub id: usize,
+    pub neighbors: Vec<usize>,
+}
 
+fn solve(input: impl Iterator<Item = String>, start: Instant) {
+    let grid = input
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let mut start = 0;
+    let mut end = 0;
+
+    let mut graph = vec![Vec::new(); grid.len()];
+
+    let len_y = grid.len();
+    let len_x = grid[0].len();
+
+    for y in 0..len_y {
+        for x in 0..len_x {
+            let mut neighbors = find_neighbors(&grid, x, y);
+            let mut height = grid[y][x] as usize;
+            if grid[y][x] == 'S' {
+                start = y * len_x + x;
+                height = 'a' as usize
+            }
+            else if grid[y][x] == 'E' {
+                end = y * len_x + x;
+                height = 'z' as usize
+            }
+
+            neighbors.retain(|neighbor| (grid[neighbor / len_x][neighbor % len_x] as usize).abs_diff(height) > 1);
+
+
+            graph[y].push(Node {
+                id: y * len_x + x,
+                neighbors
+            })
+        }
+    }
+
+    let dist = djikstra(graph.into_iter().flatten().collect(), start);
+
+    println!("{}", dist.get(&end).unwrap())
+}
+
+fn find_neighbors(grid: &Vec<Vec<char>>, x: usize, y: usize) -> Vec<usize> {
+    let mut out = Vec::new();
+    let len_y = grid.len();
+    let len_x = grid[0].len();
+    
+    if x > 1 {
+        out.push(y * len_x + x - 1);
+    }
+    if x < grid[0].len() - 1 {
+        out.push(y * len_x + x + 1);
+    }
+    if y > 1 {
+        out.push((y - 1) * len_x + x);
+    }
+    if y < grid.len() - 1 {
+        out.push((y + 1) * len_x + x)
+    }
+
+    out
+}
+
+fn djikstra(mut graph: Vec<Node>, start: usize) -> HashMap<usize, u32> {
+    let mut dist = HashMap::new();
+    let mut prev = HashMap::new();
+
+    for node in graph.iter() {
+        dist.insert(node.id, u32::MAX);
+    }
+
+    *dist.get_mut(&start).unwrap() = 0;
+
+    while !graph.is_empty() {
+        let node = graph.pop().unwrap();
+        // TODO: Find node with least distance
+        for neighbor in node.neighbors {
+            let alt = dist.get(&node.id).unwrap() + 1;
+
+            if alt < *dist.get(&neighbor).unwrap() {
+                *dist.get_mut(&neighbor).unwrap() = alt;
+                prev.insert(neighbor, node.id);
+            }
+        }
+    }
+
+    dist
 }
